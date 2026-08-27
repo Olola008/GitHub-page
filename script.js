df6c534511220e52915200be551a7cb584bb61fe
Olola008 <Olola008@osloskolen.no>
2026-08-27 14:20:13 +0200

    CLICKER

---
+++ script.js
@@ -0,0 +1,42 @@
+let poeng1 = 0;
+let poeng2 = 0;
+
+
+function pluss1(){
+    poeng1 = poeng1 +1;
+    document.getElementById("poeng1").textContent = poeng1;
+}
+
+function minus1(){
+    poeng1 = poeng1 -1;
+    document.getElementById("poeng1").textContent = poeng1;
+}
+
+function reset1(){
+ poeng1 = 0;
+ document.getElementById("poeng1").textContent = poeng1;
+}
+
+function pluss2(){
+    poeng2 = poeng2 +1;
+    document.getElementById("poeng2").textContent = poeng2;
+}
+
+function minus2(){
+    poeng2 = poeng2 -1;
+    document.getElementById("poeng2").textContent = poeng2;
+}
+
+function reset2(){
+ poeng2 = 0;
+ document.getElementById("poeng2").textContent = poeng2;
+}
+
+function darkMode(){
+    document.body.classList.toggle("mork");
+    if(document.body.classList.contains("mork")){
+        document.getElementById("darkModeButton").textContent = " ☀️Lys Modus";
+    } else {
+        document.getElementById("darkModeButton").textContent = " 🌙Mørk Modus"
+    }
+}
